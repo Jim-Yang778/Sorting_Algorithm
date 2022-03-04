54aff2e523fe16d4b493fe08f54033ba6977ef10
Jinyu Yang <jinyu.yang1@student.unsw.edu.au>
2022-03-04 13:42:01 +1100

    use std::move to avoid copy

--- algo/helper.hpp
+++ algo/helper.hpp
@@ -1,11 +1,12 @@
 #ifndef SORT_ALGORITHM_HELPER_HPP
 #define SORT_ALGORITHM_HELPER_HPP
+#include <utility>
 
 template <typename T>
 inline void swap(T &a, T &b) {
-    T tmp = a;
-    a = b;
-    b = tmp;
+    T tmp = std::move(a);
+    a = std::move(b);
+    b = std::move(tmp);
 }
 
 #endif //SORT_ALGORITHM_HELPER_HPP
